18f97dfa2093f102c44567e6e474f0d514efa648
choiseona <tjsdk617@naver.com>
2023-09-17 20:00:13 +0900

    refactor: 라우터 정리

---
+++ src/components/Router.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+
+function Router() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/posts" element={<h1>Post List Page</h1>} />
+        <Route path="/posts/:id" element={<h1>Post Detail Page</h1>} />
+        <Route path="/posts/new" element={<h1>Post New Page</h1>} />
+        <Route path="/posts/edit" element={<h1>Post Edit Page</h1>} />
+        <Route path="/profile" element={<h1>Profile Page</h1>} />
+        <Route path="*" element={<Navigate replace to="/"></Navigate>} />
+      </Routes>
+    </>
+  );
+}
+
+export default Router;
