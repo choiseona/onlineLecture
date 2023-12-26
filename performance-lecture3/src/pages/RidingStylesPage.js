b011f310ef8f879b3fc7627bbf7aa59801d26764
choiseona <tjsdk617@naver.com>
2023-12-26 13:20:44 +0900

    [feat] 이미지 사이즈 최적화

--- performance-lecture3/src/pages/RidingStylesPage.js
+++ performance-lecture3/src/pages/RidingStylesPage.js
@@ -18,25 +18,25 @@ function RidingStylesPage(props) {
 		{
 			title: "댄싱",
 			content: (
-				<YoutubePlayer id='0BwkZLJI-PI'/>
+				<YoutubePlayer id='0BwkZLJI-PI' />
 			)
 		},
 		{
 			title: "프리스타일",
 			content: (
-				<YoutubePlayer id='ybBMXQX_1_w'/>
+				<YoutubePlayer id='ybBMXQX_1_w' />
 			)
 		},
 		{
 			title: "다운힐",
 			content: (
-				<YoutubePlayer id='Qwim07syZgQ'/>
+				<YoutubePlayer id='Qwim07syZgQ' />
 			)
 		},
 		{
 			title: "프리라이딩",
 			content: (
-				<YoutubePlayer id='rO9wGssc3Dk'/>
+				<YoutubePlayer id='rO9wGssc3Dk' />
 			)
 		}
 	]
@@ -49,7 +49,7 @@ function RidingStylesPage(props) {
 				image={main_styles}
 			/>
 			<div className="container py-16 mx-auto">
-				<Tabs data={DATA}/>
+				<Tabs data={DATA} />
 			</div>
 		</div>
 	)
